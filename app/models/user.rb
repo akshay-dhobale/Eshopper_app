bdce86907c152b1077387bd17f825ddda07667b0
webwerks1 <akshay.dhobale@neosofttech.com>
2018-01-08 13:45:50 +0530

    changes in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,14 +13,11 @@ class User < ApplicationRecord
 
   after_create :welcome_user
 
-  # after_create :products_in_cart
-  # after_login :product_in_cart
-
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
       user.password = Devise.friendly_token[0,20]
-      user.firstname = auth.info.name   # assuming the user model has a name
+      # user.firstname = auth.info.name   # assuming the user model has a name
     end
   end
 
@@ -39,9 +36,6 @@ class User < ApplicationRecord
   def welcome_user
     UserMailer.user_created(self).deliver_now
   end
-  
-  def products_in_cart
-    binding.pry
-  end
+ 
 end
 
