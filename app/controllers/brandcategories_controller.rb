c2b8ef7979349a5789c8570e940af7b991059ad9
webwerks1 <akshay.dhobale@neosofttech.com>
2018-01-11 20:20:48 +0530

    indentation

--- app/controllers/brandcategories_controller.rb
+++ app/controllers/brandcategories_controller.rb
@@ -1,16 +1,16 @@
 class BrandcategoriesController < ApplicationController
-  def show
-  	@category = Category.find(params[:category_id])
-  	@brand = Brand.find(params[:brand_id])
-  	@products = @category.products.where(brand_id: @brand.id)
-  
-  	respond_to do |format|
-      format.js
-    end
-  end
+	def show
+		@category = Category.find(params[:category_id])
+		@brand = Brand.find(params[:brand_id])
+		@products = @category.products.where(brand_id: @brand.id)
+	
+		respond_to do |format|
+			format.js
+		end
+	end
 
-  private
-    def brandcategory_params
-      params.require(:brandcategory).permit(:brand_id, :category_id)
-    end
+	private
+		def brandcategory_params
+			params.require(:brandcategory).permit(:brand_id, :category_id)
+		end
 end
