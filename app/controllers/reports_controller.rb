2e66e9d41be3647aa261412c962994f3bae1d7a3
webwerks1 <akshay.dhobale@neosofttech.com>
2018-01-15 12:52:43 +0530

    changes in report

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -2,19 +2,22 @@ class ReportsController < ApplicationController
   
   def index
     @order_count = Order.group_by_day(:created_at).count
+    # @order_count = Order.where(created_at: (Time.now.midnight - 4.day)..Time.now.midnight).group_by_day(:created_at).count
     @user_count = User.group_by_day(:created_at).count
     @coupon_count = CouponsUsed.group_by_day(:created_at).count
   end
 
   def sales_report
-    @data = Order.group(:created_at).count
+    @data = Order.group_by_day(:created_at).count
   end
 
   def customer_registered
-    @data = User.group(:created_at).count
+    @data = User.group_by_day(:created_at).count
   end
 
   def coupons_used
     @data = CouponsUsed.group_by_day(:created_at).count
   end
 end
+
+
