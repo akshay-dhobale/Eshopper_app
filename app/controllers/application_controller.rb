bad1249a1799740385381cfa41cda41fbf4e0bee
webwerks1 <akshay.dhobale@neosofttech.com>
2018-01-15 11:14:30 +0530

    404 page changed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,6 @@
 class ApplicationController < ActionController::Base
  
   protect_from_forgery with: :exception
-  rescue_from ::ActionController::RoutingError, with: :render_not_found
   
   def new_session_path(scope)
     new_user_session_path
@@ -11,10 +10,4 @@ class ApplicationController < ActionController::Base
     request.env['omniauth.origin'] || root_path
   end
 
-  def render_not_found(exception)
-  	binding.pry
-  	respond_to do |f| 
-      f.html{ render :template => "404", :status => 404 }
-    end
-  end
 end
