207a9a13b00ca0a6804b9625038a02137c1ed124
webwerks1 <akshay.dhobale@neosofttech.com>
2018-01-16 17:49:47 +0530

    small changes removed comments and useless file

--- config/routes.rb
+++ config/routes.rb
@@ -19,13 +19,6 @@ Rails.application.routes.draw do
 
   root 'home#index'
   
-  # get 'addresses/index'
-
-  # get 'addresses/show'
-
-  # get 'addresses/edit'
-  
-  # get 'addresses/destroy'
   resources :addresses
 
   get 'carts/index'
@@ -37,8 +30,6 @@ Rails.application.routes.draw do
   get 'carts/update'
 
   post 'carts/add_coupon', to:'carts#add_coupon'
-
-  # get 'subcategories/index'
 
   get 'subcategories/show'
 
@@ -53,29 +44,16 @@ Rails.application.routes.draw do
 
   get 'brandcategories/show'
 
-  # get 'subcategories/show'
   resources :addresses
-  # get "/fetch_items" => 'items#from_category', as: 'fetch_items'
 
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
-  # devise_scope :user do
-  #   delete 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
-  # end
-
-  
-  # get "users/show"
-  # get "users/edit"
-  
-  # resources :users
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   
   resources :charges
 
   resources :banners
 
-  # resources :checkouts
-  # get 'categories/show'
   resources :categories
 
   resources :subcategories
@@ -88,17 +66,12 @@ Rails.application.routes.draw do
     get 'enter_email'
   end
 
-  # resources :newsletters
   resources :contact_us
 
   resources :cms
+  
   resources :orders do 
-  # post 'orders/create'
-  # get 'orders/show'
     get 'details', on: :member
   end
   
-  # resources :carts
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
